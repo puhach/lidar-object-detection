b995ae2c6242ba2223f43659f435a5ec0e70ac77
Yaroslav Puhach <yaroslav.puhach@intellias.com>
2022-04-22 17:50:47 +0300

    Remove clusters which are too small or too large

--- src/euclidean_clusterer.hpp
+++ src/euclidean_clusterer.hpp
@@ -1,17 +1,19 @@
 
 template <typename PointT>
-//bool EuclideanClusterer<PointT>::expandCluster(std::size_t pointIndex)
 bool EuclideanClusterer<PointT>::expandCluster(const PointTreeItem &item)
 {
+    // Check whether the point already belongs to a cluster
     auto clusterIndex = this->clusterAffinity[item.index];
     if (clusterIndex >= 0)
-        return false;     // already in a cluster
+        return false;     
 
-    this->clusterAffinity[item.index] = numClusters;
+    // Assign a cluster index to this point. The current cluster index is the same as the number of clusters we have completely expanded.
+    this->clusterAffinity[item.index] = this->numClusters;
 
-    //const auto& neighborIndices = this->tree.search(this->cloud->at(pointIndex), this->distanceTolerance);
+    // Find nearby points
     const auto& neighbors = this->tree.search(item, this->distanceTolerance);
 
+    // Add neighboring points to the same cluster
     for (const auto &neighbor : neighbors)
     {
         expandCluster(neighbor);
@@ -21,37 +23,29 @@ bool EuclideanClusterer<PointT>::expandCluster(const PointTreeItem &item)
 }   // expandCluster
 
 template <typename PointT>
-std::vector<typename pcl::PointCloud<PointT>::Ptr> EuclideanClusterer<PointT>::clusterize(const typename pcl::PointCloud<PointT>::ConstPtr &cloud)
-//std::vector<typename pcl::PointCloud<PointT>::Ptr> EuclideanClusterer<PointT>::clusterize(typename pcl::PointCloud<PointT>::ConstPtr cloud)
+std::vector<typename pcl::PointCloud<PointT>::Ptr> EuclideanClusterer<PointT>::clusterize(const typename pcl::PointCloud<PointT>::ConstPtr& cloud)
 {
     this->numClusters = 0;
-    this->cloud = cloud; //std::move(cloud);
-    //KdTree<PointT> kdTree;
+    this->cloud = cloud; 
     this->tree.clear();
 
-    //for (const auto& p : *cloud)
+    // Add all points to the tree
     for (std::size_t i = 0; i < cloud->size(); ++i)
     {
-        //this->tree.insert(i, cloud->at(i));
         this->tree.insert(EuclideanClusterer<PointT>::PointTreeItem{ i, cloud->at(i) });
     }
 
-    //std::size_t numClusters = 0;
-    //std::vector<std::make_signed_t<std::size_t>> clusterAffinity(cloud->size(), -1);
+    // Map each point to a cluster index
     this->clusterAffinity.resize(cloud->size());
     std::fill(this->clusterAffinity.begin(), this->clusterAffinity.end(), -1);
-
     for (std::size_t i = 0; i < cloud->size(); ++i)
     {
         EuclideanClusterer<PointT>::PointTreeItem item{ i, this->cloud->at(i) };
-
-        //if (expandCluster(i))
         if (expandCluster(item))
             ++this->numClusters;
-
     }   // for i
 
-
+    // Group points by their cluster indices
     assert(clusterAffinity.size() == cloud->size());
     std::vector<typename pcl::PointCloud<PointT>::Ptr> clusters(numClusters);
     std::generate(clusters.begin(), clusters.end(), []() { return pcl::PointCloud<PointT>::Ptr(new pcl::PointCloud<PointT>); });
@@ -63,16 +57,12 @@ std::vector<typename pcl::PointCloud<PointT>::Ptr> EuclideanClusterer<PointT>::c
         clusters[clusterIndex]->push_back(cloud->at(i));
     }   // for i
 
-    //std::vector<typename pcl::PointCloud<PointT>::Ptr> clusters;    // TODO: use numClusters
-    //for (std::size_t i = 0; i < this->clusterAffinity.size(); ++i)
-    //{
-    //    auto clusterIndex = this->clusterAffinity[i];
-    //    assert(clusterIndex >= 0);
-    //    //auto& cluster = clusterIndex < clusters.size() ? clusters[clusterIndex] : clusters.emplace_back(new pcl::PointCloud<PointT>);
-    //    if (clusterIndex >= clusters.size())
-    //        clusters.emplace_back(new pcl::PointCloud<PointT>);
-    //    clusters.back()->push_back(cloud->at(i));
-    //}   // for i
+    // Remove clusters which are too small or too large
+    clusters.erase(std::remove_if(clusters.begin(), clusters.end(),
+        [this](const auto& cluster)
+        {
+            return cluster->size() < this->minClusterSize || cluster->size() > this->maxClusterSize;
+        }), clusters.end());
 
     return clusters;
 }   // clusterize
